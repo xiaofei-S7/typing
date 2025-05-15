c94a5bad9ed486a136da587b35c8fec3846e5ff9
AKHJDSGBBJC <ryanzhao48@gmail.com>
2025-05-15 16:03:21 -0400

    Update java.js

--- typing game Ryan/java.js
+++ typing game Ryan/java.js
@@ -1,12 +1,36 @@
-const Random_API_URL = 'https://sentence.underthekey.com/random?count=1'
+const Random_API_URL = 'https://sentence.underthekey.com/language?language=eng&count=1'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 
 
+quoteInputElement.addEventListener('input',() => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index] 
+if (character == null){
+    characterSpan.classList.remove('correct')
+    characterSpan.classList.remove('inncorrect')
+}
+        if (character === characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+
+        }
+    })
+
+    if (correct) renderNewQuote()
+})
+
 function getRandomQuote() {
     return fetch(Random_API_URL)
     .then(response => response.json())
-    .then(data=> data.join(''))
+    .then(data=> data[0].content)
 }
 
 async function renderNewQuote() {
